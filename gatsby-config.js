15538cc8010a2375b3668bc9d5eec998112e278d
tupobo <1028461931@qq.com>
2020-05-09 22:45:06 +0800

    使用MDX

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,5 +13,23 @@ module.exports = {
     title: "Personal Bolg",
     description: "Personal Blog",
   },
-  plugins: ["gatsby-plugin-emotion", `gatsby-plugin-react-helmet`],
+  plugins: [
+    "gatsby-plugin-emotion",
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/layout"),
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `posts`
+      }
+    }
+  ],
 }
