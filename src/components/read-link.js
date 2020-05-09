7b98f98fe76871825003f2e1f9446db462e63300
tupobo <1028461931@qq.com>
2020-05-09 23:26:48 +0800

    渲染博客文章列表

---
+++ src/components/read-link.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Link } from "gatsby"
+import styled from "@emotion/styled"
+
+const ReadLink = styled(Link)`
+  display: inline-block;
+  font-size: 0.75rem;
+`
+export default ReadLink
